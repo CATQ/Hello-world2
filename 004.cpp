01f9c29e68bff8253b773af4ade381b222673685
CATQ <819735890@qq.com>
2017-10-10 09:11:58 +0800

    homework

---
+++ 004.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+int a,b,c,d,m,n;
+    double x=0,y=0;
+    scanf("%d,%d,%d,%d,%d,%d",&a,&b,&c,&d,&m,&n);
+   // if(0==1)printf("нч╫Б");
+   // else{ 
+	     x=(m*d-b*n)/(a*d-b*c);
+         y=(m*c-a*n)/(b*c-a*d);
+         printf("x=%f,y=%f",x,y); 
+   //    }
+    return 0;
+}
